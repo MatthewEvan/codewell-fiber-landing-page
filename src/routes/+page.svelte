dde6c6d30a5f43683858e406906a70eea8904227
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-04-20 14:31:37 +0000

    Html structuring and basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,37 @@
 <script>
-  import Nav from "./components/nav.svelte"
+  // components importing
+	import Nav from './components/nav.svelte';
+
+  // src and links
+
+  // logic and functions
 </script>
 
-<h1>Welcome to Fibre</h1>
-<p>Website Prototype of Fibre: Online Portfolio Maker by Matthew</p>
-<p>This is only a prototype for me to discover svelte and practice layouting</p>
-<Nav/>
+<body>
+	<header><Nav /></header>
+	<main>
+		<h1>Welcome to Fibres</h1>
+		<p>Website Prototype of Fibre: Online Portfolio Maker by Matthew</p>
+		<p>This is only a prototype for me to discover svelte and practice layouting</p>
+	</main>
+	<footer></footer>
+</body>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-  * {
-    font-family: "Inter", sans-serif;
-  }
+	* {
+		padding: 0;
+		margin: 0;
+		font-family: 'Inter', sans-serif;
+		box-sizing: border-box;
+		font-size: 14px;
+	}
+
+	/* a {
+		text-decoration: none;
+	} */
+
+	body {
+		font-family: 'Inter', sans-serif;
+	}
 </style>
