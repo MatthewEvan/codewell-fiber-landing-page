1c9202b7186e9784568411a7ac15d7236785f414
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-04-20 13:49:12 +0000

    basic routing and component testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
+<script>
+  import Nav from "./components/nav.svelte"
+</script>
+
 <h1>Welcome to Fibre</h1>
 <p>Website Prototype of Fibre: Online Portfolio Maker by Matthew</p>
 <p>This is only a prototype for me to discover svelte and practice layouting</p>
+<Nav/>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
